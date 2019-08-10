22127f720ae41f686e4bc34981f82d86a40b4afd
Shehanka <hcsperera@outlook.com>
2019-08-10 17:15:44 +0530

    Model classed added

--- herewego.go
+++ herewego.go
@@ -1,13 +1,51 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 	"math/rand"
 	"strconv"
 	"github.com/gorilla/mux"
 )
+
+// Author Struct
+type Author struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
+// Book Struct (Model)
+type Book struct {
+	ID     string  `json:"id"`
+	Isbm   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+// Get all books
+func getBooks(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Get single book
+func getBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Create a new book
+func createBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Update a book
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Remove a Book
+func removeBook(w http.ResponseWriter, r *http.Request) {
+
+}
 
 func main() {
 	// Init Router
@@ -19,4 +57,6 @@ func main() {
 	r.HandleFunc("/api/books", createBook).Methods("POST")
 	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
 	r.HandleFunc("/api/books/{id}", removeBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
